216c3f5183d2cf19e2e06ef17ef2cb94e0e2f6d4
fanka <fanka@bk.ru>
2017-05-29 12:10:26 +0200

    Cloudinary installed

--- config/initializers/cloudinary.rb
+++
@@ -1,6 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = 'fanka'
-  config.api_key = '688689256329436'
-  config.api_secret = '_PIMOq5FLEV9nI0vUnbETNokZYU'
-  config.cdn_subdomain = true
-end
